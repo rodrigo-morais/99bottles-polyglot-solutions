ce5ad000cc8f0b99f97e66f52806f535766d4b0c
rodrigo-morais <morais.rm@gmail.com>
2017-12-21 21:15:12 +0100

    Adding solution to elm.

--- elm/src/BeerSong.elm
+++ elm/src/BeerSong.elm
@@ -1,13 +1,36 @@
 module BeerSong exposing (verse, verses, sing)
 
+import List exposing (range, reverse)
+
 verse : Int -> String
 verse number =
-  ""
+  let
+    current = toString(number)
+    previous = toString(number - 1)
+  in
+    case number of
+      0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+      1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      2 -> "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      _ -> current ++ " bottles of beer on the wall, " ++ current ++ " bottles of beer.\nTake one down and pass it around, " ++ previous ++ " bottles of beer on the wall.\n"
+
+
+combineVerses : List Int -> String
+combineVerses versesNum =
+  case versesNum of
+    (x::[]) -> verse x
+    (x::xs) -> verse x ++ "\n" ++ combineVerses xs
+    [] -> ""
+
 
 verses : Int -> Int -> String
-verses begin end =
-  ""
+verses end begin =
+  let
+    versesNum = reverse <| range begin end
+  in
+    combineVerses versesNum
+
 
 sing : String
 sing =
-  ""
+  verses 99 0
