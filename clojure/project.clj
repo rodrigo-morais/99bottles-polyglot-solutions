0e66899153d01d49ea315962dc3730b97c389253
rodrigo-morais <morais.rm@gmail.com>
2017-12-28 18:19:32 +0100

    Adding solution to Clojure with `core.match`.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,6 +1,7 @@
 (defproject beer-song "0.1.0"
   :description "Beer song challenge"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                [org.clojure/core.match "0.3.0-alpha5"]]
   :main ^:skip-aot beer-song
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- clojure/src/beer_song.clj
+++ clojure/src/beer_song.clj
@@ -1,4 +1,5 @@
-(ns beer-song)
+(ns beer-song
+(:require [clojure.core.match :refer [match]]))
 
 (defn verse-case
   [number]
@@ -33,6 +34,18 @@
   "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"))
 (defmethod verse-multimethod :default [number] (str (str number) " bottles of beer on the wall, " (str number) " bottles of beer.\n"
   "Take one down and pass it around, " (str (- number 1)) " bottles of beer on the wall.\n\n"))
+
+(defn verse-match
+  [number]
+  (match number
+    0 (str "No more bottles of beer on the wall, no more bottles of beer.\n"
+       "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n")
+    1 (str "1 bottle of beer on the wall, 1 bottle of beer.\n"
+       "Take it down and pass it around, no more bottles of beer on the wall.\n\n")
+    2 (str "2 bottles of beer on the wall, 2 bottles of beer.\n"
+       "Take one down and pass it around, 1 bottle of beer on the wall.\n\n")
+    :else (str (str number) " bottles of beer on the wall, " (str number) " bottles of beer.\n"
+       "Take one down and pass it around, " (str (- number 1)) " bottles of beer on the wall.\n\n")))
 
 (defn verse
   [number]
